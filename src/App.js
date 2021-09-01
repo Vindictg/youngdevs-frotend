f1a6784519ea6d6416c7dcffe115ffc43111a299
jrodriguezch <josegk.rodriguezc@gmail.com>
2021-09-01 17:00:32 -0300

    initial config and walking skeleton login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
+
+import firebaseConfig from "./FirebaseConfig";
+
 import './App.css';
 
+initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
 function App() {
+  const [user, setUser] = useState(auth.currentUser);
+
+  onAuthStateChanged(auth, (user) => {
+    setUser(user);
+  });
+
+  const googleLogin = () => {
+    signInWithRedirect(auth, provider);
+  }
+
+  const logout = () => {
+    auth.signOut();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          user ?
+          (
+            <>
+              <div>{ user.email }</div>
+              <button
+                className="App-link"
+                onClick={logout}
+              >
+                Logout
+              </button>
+            </>
+          )
+          :
+          (
+            <button
+              className="App-link"
+              onClick={googleLogin}
+            >
+              Google Login
+            </button>
+          )
+        }
       </header>
     </div>
   );
