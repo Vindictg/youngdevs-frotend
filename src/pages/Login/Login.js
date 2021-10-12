b94fb719e92ea5827a0f07e9656285d546360436
miguelapabenedit <miguell.beneditt@gmail.com>
2021-10-11 22:29:15 -0300

    Add Material Ui

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import useAuth from '../../hooks/useAuth';
 
 function Login() {
@@ -15,7 +16,7 @@ function Login() {
           <h1>YoungDevs</h1>
           <h3>Learning to code has never been so easy, join thousands of students learn,</h3>
           <h3>compare and have fun with others.</h3>
-          <button type="button" className="App-link" onClick={handleLogIn}>Google Login</button>
+          <Button variant="contained" className="App-link" onClick={handleLogIn}>Google Login</Button>
         </div>
       </div>
     </div>
