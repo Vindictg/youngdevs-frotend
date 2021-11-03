34d3a21abf04f641ceaf2f2157ae29cea1c0762f
belenmr <mbmedina@indracompany.com>
2021-11-03 03:22:45 -0300

    Modify Ranking

--- src/pages/Ranking/Ranking.js
+++ src/pages/Ranking/Ranking.js
@@ -1,4 +1,5 @@
-import { Typography } from '@material-ui/core';
+// eslint-disable-next-line object-curly-newline
+import { Typography, TableRow, TableHead, TableBody, TableCell, TableContainer } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Ranking.scss';
@@ -17,20 +18,24 @@ function Ranking() {
         <div className="Ranking-content">
           <Typography variant="h3">RANKING</Typography>
           <div className="Ranking-table Home-link-content">
-            { users?.map((u, i) => (
-              <div>
-                <Typography variant="h6" key={u.ID}>
-                  {i + 1}
-                  .-
-                  {' '}
-                  {u.Name}
-                  {' '}
-                  _________
-                  {u.Score}
-                </Typography>
-                <br />
-              </div>
-            ))}
+            <TableContainer>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center"><Typography variant="h5">Position</Typography></TableCell>
+                  <TableCell align="center"><Typography variant="h5">Username</Typography></TableCell>
+                  <TableCell align="center"><Typography variant="h5">Score</Typography></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                { users?.map((u, i) => (
+                  <TableRow key={u.ID}>
+                    { i < 3 ? <TableCell align="center"><div className="Ranking-top">{i + 1}</div></TableCell> : <TableCell align="center">{i + 1}</TableCell> }
+                    { i < 3 ? <TableCell align="center"><div className="Ranking-top">{u.Name}</div></TableCell> : <TableCell align="center">{u.Name}</TableCell> }
+                    { i < 3 ? <TableCell align="center"><div className="Ranking-top">{u.Score}</div></TableCell> : <TableCell align="center">{u.Score}</TableCell> }
+                  </TableRow>
+                ))}
+              </TableBody>
+            </TableContainer>
           </div>
           <br />
           <br />
