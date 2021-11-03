7890f557083a1cfc9a398404bf535ef20f29d193
belenmr <mbmedina@indracompany.com>
2021-11-03 15:44:34 -0300

    Add Loading Skeleton Ranking

--- src/pages/Ranking/Ranking.js
+++ src/pages/Ranking/Ranking.js
@@ -3,15 +3,70 @@ import { Typography, TableRow, TableHead, TableBody, TableCell, TableContainer }
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Ranking.scss';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import UserProvider from '../../providers/UserProvider';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 function Ranking() {
   const [users, setUsers] = useState();
+  const [loading, setLoading] = useState(true);
+
+  // eslint-disable-next-line arrow-body-style
+  const loader = () => {
+    const rows = [];
+    // eslint-disable-next-line no-plusplus
+    for (let index = 0; index < 3; index++) {
+      rows.push(
+        <TableRow key={index}>
+          <TableCell align="center"><Skeleton /></TableCell>
+          <TableCell align="center"><Skeleton /></TableCell>
+          <TableCell align="center"><Skeleton /></TableCell>
+        </TableRow>,
+      );
+    }
+    return (
+      <div className="App">
+        <div className="App-container">
+          <div className="Ranking-content">
+            <SkeletonTheme baseColor="gray" highlightColor="black" borderRadius="0.5rem">
+              <Typography variant="h3">RANKING</Typography>
+              <div className="Ranking-table Home-link-content">
+                <TableContainer>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell align="center"><Typography variant="h5">Position</Typography></TableCell>
+                      <TableCell align="center"><Typography variant="h5">Username</Typography></TableCell>
+                      <TableCell align="center"><Typography variant="h5">Score</Typography></TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    { rows }
+                  </TableBody>
+                </TableContainer>
+              </div>
+              <br />
+              <br />
+              <Link className="App-link" to="/">BACK TO MENU</Link>
+            </SkeletonTheme>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
   useEffect(async () => {
     const u = await UserProvider.getRanking();
     setUsers(u);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
   }, []);
+
+  if (loading) {
+    return (
+      loader()
+    );
+  }
   return (
     <div className="App">
       <div className="App-container">
