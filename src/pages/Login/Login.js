8dab1fab3a81fb16f590687b793e2eb13087f8aa
belenmr <mbmedina@indracompany.com>
2021-11-07 18:01:15 -0300

    Add loading skeleton Login Page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Typography } from '@material-ui/core';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import useAuth from '../../hooks/useAuth';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 function Login() {
   const { logIn } = useAuth();
+  const [loading, setLoading] = useState(false);
+  // eslint-disable-next-line arrow-body-style
+  const loader = () => {
+    return (
+      <div className="App">
+        <div className="App-container">
+          <div className="Login-content">
+            <SkeletonTheme baseColor="gray" highlightColor="black" borderRadius="0.5rem">
+              <Typography variant="h3">YoungDevs</Typography>
+              <br />
+              <Typography variant="h4"><Skeleton /></Typography>
+            </SkeletonTheme>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
   const handleLogIn = async () => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 1000);
     logIn();
   };
 
+  if (loading) {
+    return (
+      loader()
+    );
+  }
   return (
     <div className="App">
       <div className="App-container">
